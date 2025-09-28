3f8431f91456101dc2818ceb51b5cdc3e694c717
MuhammadDevs <deenmuhammad19n@gmail.com>
2025-09-28 11:31:28 +0600

    added navbar and foooter section

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div>
+                <p>Books to freshen up your bookshelf</p>
+                <button className='btn btn-primary'>View The List</button>
+            </div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Banner;
